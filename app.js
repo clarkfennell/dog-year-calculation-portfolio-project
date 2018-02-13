730169b1c18dc26f4225117ad86ef217c328c4ca
Clark Fennell <35297195+clarkfennell@users.noreply.github.com>
2018-02-13 14:13:39 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const myAge = 29;
+
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+
+laterYears *= 4;
+
+let myAgeInDogYears = earlyYears + laterYears;
+
+const myName = 'Clark'.toLowerCase();
+
+console.log(`My name is ${myName} and I am ${myAge} years old. I am ${myAgeInDogYears} years old in dog years.`);
